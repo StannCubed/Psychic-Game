db5d4a54325c6c2eff7a516570d084f1d91e7819
Tristan Luer <tristan@Tristans-MacBook-Pro.local>
2017-02-18 09:57:29 -0800

    modify javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,23 @@
-var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
 var guessCounter;
 var winCounter;
 var lossCounter;
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+var guesses = [];
+
+
 var randomLetter = Math.floor(Math.random() * 25);
-var guessLetter = document.getElementById("guessWhich")
+
+document.getElementById("guessWhich").textContent = guessCounter
+document.getElementById("wins").textContent = winCounter
+document.getElementById("losses").textContent = lossCounter
 
 document.onkeyup = function () {
+	guesses.push = event.key;
+}
 
+if (guesses[guesses.length] = randomLetter) {
+	winCounter++;
 }
 
 for (guessCounter = 9; i === 0; i--) {
