9ccccf3af36d332fd2d1b2049b0ea79b4346f8d1
Tristan Luer <tristan@Tristans-MacBook-Pro.local>
2017-02-18 09:41:23 -0800

    modify javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+var guessCounter;
+var winCounter;
+var lossCounter;
+var randomLetter = Math.floor(Math.random() * 25);
+var guessLetter = document.getElementById("guessWhich")
+
+document.onkeyup = function () {
+
+}
+
+for (guessCounter = 9; i === 0; i--) {
+	if (i === 0) {
+		lossCounter++;
+		guessCounter = 9;
+	}
+
+}
+
